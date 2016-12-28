a13ff45550a218403817685b21baaf6018b0f37a
skiwi <frank_skiwi@hotmail.com>
2016-12-28 17:55:34 +0100

    Added basic ManaCostSelectorControl and added it to the ArenaDraftView for mana costs 1, 2, 3, 4, 5 and 6.

---
+++ ArenaDraftAssistant/ManaCostSelectorControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArenaDraftAssistant
+{
+    /// <summary>
+    /// Interaction logic for ManaCostSelector.xaml
+    /// </summary>
+    public partial class ManaCostSelectorControl : UserControl
+    {
+        public ManaCostSelectorControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty ManaCostProperty = DependencyProperty.Register(nameof(ManaCost), typeof(int), typeof(ManaCostSelectorControl));
+
+        public int ManaCost
+        {
+            get { return (int) GetValue(ManaCostProperty); }
+            set { SetValue(ManaCostProperty, value); }
+        }
+    }
+}
